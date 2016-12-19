c475b10d6cfb5afb58b61746f23a8488aa924417
Daniel jimenez <daniel.jimenez@domoti-sas.com>
2016-12-18 21:24:57 -0500

    primer intento cms dinamico

--- public/index.js
+++ public/index.js
@@ -17,12 +17,99 @@
 */
 
 var app = angular.module('dmt-back', ['ngMaterial']);
-app.config(function ($mdThemingProvider,$route) {
+app.config(function ($mdThemingProvider) {
 	$mdThemingProvider.theme('default')
 		.primaryPalette('blue')
 		.accentPalette('grey');
 });
 
-app.controller('apisController', function ($http) {
-	
+app.controller('backCtrl', function ($scope) {
+	var leftMenu=[
+		{
+			section:"Configuración",
+			items:[{
+				item:"Ciudades",
+				table:"city",
+				fields:[
+					{
+						name:"id",
+						type:"int",
+						editable:"false"
+					},
+					{
+						name:"name",
+						type:"string",
+						editable:"true"
+					},
+					{
+						name:"code",
+						type:"string",
+						editable:"true"
+					},
+					{
+						name:"id_country",
+						type:"link",
+						editable:"true",
+						table:"country",
+						foreign_key:"id"
+					}
+				]
+			},
+			{
+				section:"Foro",
+				item:"Region",
+				table:"region",
+				fields:[
+					{
+						name:"id",
+						type:"int",
+						editable:"false"
+					},
+					{
+						name:"name",
+						type:"string",
+						editable:"true"
+					},
+					{
+						name:"id_capital",
+						type:"link",
+						editable:"true",
+						table:"city",
+						foreign_key:"id"
+					}
+				]
+			}]
+		},
+		{
+			section:"Foro",
+			items:[{
+				item:"Temas",
+				table:"topic",
+				fields:[
+					{
+						name:"id",
+						type:"int",
+						editable:"false"
+					},
+					{
+						name:"name",
+						type:"string",
+						editable:"true"
+					},
+					{
+						name:"id_parent",
+						type:"link",
+						editable:"true",
+						table:"topic",
+						foreign_key:"id"
+					}
+				]
+			}]
+		}
+	];
+
+	$scope.leftMenu = leftMenu;
+	$scope.selectItem = function(i){
+		$scope.currentItem = i;
+	}
 });
