272dbe31e4448430dafaf7eb501ca74b90ace540
Jorge Ivan Andres Contreras Pereira <jorge@contreras.com.co>
2016-12-27 17:30:11 -0500

    update script new version

--- deploy/new_version.sh
+++ deploy/new_version.sh
@@ -1,5 +1,5 @@
 #¡/bin/bash
-export VERSION=$(cat VERSION)
+export VERSION=$(cat deploy/VERSION)
 ARRAY_VERSION=(${VERSION//./ })
 VERSION_MAJOR=${ARRAY_VERSION[0]}
 VERSION_MINOR=${ARRAY_VERSION[1]}
@@ -8,13 +8,13 @@ VERSION_NEW_CONSECUTIVE=VERSION_CONSECUTIVE
 VERSION_NEW_MINOR=VERSION_MINOR
 let VERSION_NEW_CONSECUTIVE+=1   
 echo "Se subirá de version ${VERSION} a ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_NEW_CONSECUTIVE}"
-echo ${VERSION_MAJOR}.${VERSION_NEW_MINOR}.${VERSION_NEW_CONSECUTIVE} > VERSION
-echo ${VERSION_MAJOR}-${VERSION_NEW_MINOR}-${VERSION_NEW_CONSECUTIVE} > VERSION_UNDERSCORE
+echo ${VERSION_MAJOR}.${VERSION_NEW_MINOR}.${VERSION_NEW_CONSECUTIVE} > deploy/VERSION
+echo ${VERSION_MAJOR}-${VERSION_NEW_MINOR}-${VERSION_NEW_CONSECUTIVE} > deploy/VERSION_UNDERSCORE
 
 #Preparando mensaje de SLACK
-export SLACK_CHANNEL=$(cat slack/CHANNEL)
-export CLOUDSDK_CORE_PROJECT=$(cat gcp/CLOUDSDK_CORE_PROJECT)
+export SLACK_CHANNEL=$(cat deploy/slack/CHANNEL)
+export CLOUDSDK_CORE_PROJECT=$(cat deploy/gcp/CLOUDSDK_CORE_PROJECT)
 export URL_DEPLOY=https://${VERSION_MAJOR}-${VERSION_MINOR}-${VERSION_NEW_CONSECUTIVE}-dot-${CLOUDSDK_CORE_PROJECT}.appspot-preview.com
-echo "curl -X POST --data-urlencode 'payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"DOMOTI\", \"text\": \"Se ha desplegado una nueva version de ${BRANCH} en ${URL_DEPLOY}\"}' ${SLACK_WEBHOOKS}" > slack/slack.sh
-chmod +x slack/slack.sh
+echo "curl -X POST --data-urlencode 'payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"DOMOTI\", \"text\": \"Se ha desplegado una nueva version de ${BRANCH} en ${URL_DEPLOY}\"}' ${SLACK_WEBHOOKS}" > deploy/slack/slack.sh
+chmod +x deploy/slack/slack.sh
             
