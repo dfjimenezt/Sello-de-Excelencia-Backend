cefdbc5778562a20231c36d7384fe73a6a1ec44d
carolinaPG <crpulidog@unal.edu.co>
2017-08-13 23:19:12 -0500

    cuando comento el gitignore

--- webservices_pruebas/mysql/init.sql
+++ webservices_pruebas/mysql/init.sql
@@ -55,42 +55,38 @@ TRUNCATE TABLE `user_role`;
 #requisito = requisito + sustentación legal + justificación + criterio + evidencia + ayuda + adjunto
 
 /**
-*   Administrador
+*   Administrador, id_user = 1
 */
-# Administrador , id_user = 1
 INSERT INTO user (`id`, `name`, `email`, `active`, `verified`, `password`) VALUES ('1', 'Administrador', 'carolina.pulido@domoti-sas.com', '1', '1', '4c71778a7acf11690030fcddd4a27a1dae7c11cb54770dfe6560a5621335b7eb');
 INSERT INTO user_role (`id_user`,`id_role`) VALUES ('1','3');
 
 /**
-*   Ciudadanos
+*   Ciudadano 1, id_user = 2
 */
-# Ciudadano 1 , id_user = 2
 INSERT INTO user (`id`, `name`, `secondname`, `lastname`, `secondlastname`, `email`, `active`, `verified`, `password`, `tmp_pwd`) VALUES ('2', 'Carolina', 'Rosa', 'Pulido', 'Gómez', 'caropugo92@gmail.com', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '0');
 INSERT INTO user_role (`id_user`,`id_role`) VALUES ('2','1');
 
 /**
-*   Evaluadores
+*   Evaluador 1, id_user = 3
 */
-# Evaluador 1 , id_user = 3
-INSERT INTO user (`id`, `name`, `secondname`, `lastname`, `secondlastname`, `email`, `phone`, `mobile`, `active`, `verified`, `password`, `tmp_pwd`, `flag_hall`, `ranking_hall`, `points`, `document`, `id_type_document`, `id_availability`, `id_level`, `id_city`, `education_level`, `ocupation`, `organization`) VALUES ('3', 'Johnny', 'Germán', 'Cubides', 'Castro', 'johnny.cubides@domoti-sas.com', '0312456789', '3101234567', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '0', '0', '0', '10', '1101175789', '1', '1', '2', '1', 'Universitario', 'Desempleado', 'unal');
+INSERT INTO user (`id`, `name`, `secondname`, `lastname`, `secondlastname`, `email`, `phone`, `mobile`, `active`, `verified`, `password`, `tmp_pwd`, `points`, `document`, `id_type_document`, `id_availability`, `education_level`, `ocupation`, `organization`, `id_country`, `id_city`, `id_region`) VALUES ('3', 'Carolina', 'Evaluador', '', '', 'crpulidog0@gmail.com', '0312456789', '3101234567', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '0', '10', '1101175789', '1', '1', 'Universitario', 'Desempleado', 'unal', '42', '3136', '33');
 INSERT INTO user_role (`id_user`,`id_role`) VALUES ('3','2');
 
 /**
-*   Entidades
+*   Entidad 2, id_user = 4
 */
-# Entidad 1 , id_user = 4
-INSERT INTO user (`id`, `name`, `email`, `active`, `verified`, `password`, `tmp_pwd`, `flag_hall`, `ranking_hall`, `points`, `terms`) VALUES ('4', 'Entidad1', 'santiago.alvarez@domoti-sas.com', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '1', '0', '0', '15', '1');
-# La siguiente línea es la correcta
-#INSERT INTO institution (`id`, `name`, `nit`, `email`, `phone`, `legalrep_name`, `legalrep_secondname`, `legalrep_lastname`, `legalrep_secondlastname`, `legalrep_typedoc`, `legalrep_document`, `legalrep_email`) VALUES ('1', 'Entidad1', '22334455', 'santiago.alvarez@domoti-sas.com', '3101111111', 'Pepito', 'Ramón', 'Perez', 'Espitia', '1', '1010000000', 'santiago@gmail.com');
-INSERT INTO institution (`id`, `name`, `nit`, `email`, `phone`, `extension`, `legalrep_name`, `legalrep_secondname`, `legalrep_lastname`, `legalrep_secondlastname`, `legalrep_typedoc`, `leaglrep_document`, `legalrep_email`, `id_region`, `id_city`) VALUES ('1', 'Entidad1', '22334455', '12332', 'santiago.alvarez@domoti-sas.com', '3101111111', 'Pepito', 'Ramón', 'Perez', 'Espitia', '1', '1010000000', 'santiago@gmail.com', '', '');
+INSERT INTO user (`id`, `name`, `email`, `active`, `verified`, `password`, `tmp_pwd`, `points`, `terms`,  `id_region`, `id_city`) VALUES ('4', 'Entidad2', 'crpulidog1@gmail.com', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '1', '13', '1',  '1', '3106');
+INSERT INTO institution (`id`, `name`, `nit`, `email`, `phone`, `extension_phone`, `legalrep_name`, `legalrep_secondname`, `legalrep_lastname`, `legalrep_secondlastname`, `legalrep_typedoc`, `legalrep_document`, `legalrep_email`, `id_region`, `id_city`) VALUES ('1', 'Entidad2', '22334455', '12332', 'crpulidog2@gmail.com', '3101111111', 'Pepito', 'Ramón', 'Perez', 'Espitia', '1', '1010000000', 'santiago@gmail.com', '1', '3106');
 INSERT INTO institution_user (`id_institution`,`id_user`) VALUES ('1', '4');
+INSERT INTO user_role (`id_user`,`id_role`) VALUES ('4','4');
 
-# Entidad 2 , id_user = 5
-INSERT INTO user (`id`, `name`, `email`, `active`, `verified`, `password`, `tmp_pwd`, `flag_hall`, `ranking_hall`, `points`, `terms`) VALUES ('5', 'Entidad2', 'carolina.pulido@domoti-sas.com', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '1', '0', '0', '10', '1');
-# La siguiente línea es la correcta
-#INSERT INTO institution (`id`, `name`, `nit`, `email`, `phone`, `extension`, `legalrep_name`, `legalrep_secondname`, `legalrep_lastname`, `legalrep_secondlastname`, `legalrep_typedoc`, `leaglrep_document`, `legalrep_email`, `id_region`, `id_city`) VALUES ('2', 'Entidad2', '11111155', '10302', 'carolina.pulido@domoti-sas.com', '3100001111', 'Rosa', 'María', 'Sepúlveda', 'Torres', '1', '1010111222', 'crpulidog1@gmail.com', '3', '1054');
-INSERT INTO institution (`id`, `name`, `nit`, `email`, `phone`, `legalrep_name`, `legalrep_secondname`, `legalrep_lastname`, `legalrep_secondlastname`, `legalrep_typedoc`, `leaglrep_document`, `legalrep_email`, `id_region`, `id_city`) VALUES ('2', 'Entidad2', '11111155', 'carolina.pulido@domoti-sas.com', '3100001111', 'Rosa', 'María', 'Sepúlveda', 'Torres', '1', '1010111222', 'crpulidog1@gmail.com', '3', '1054');
-INSERT INTO institution_user (`id_institution`,`id_user`) VALUES ('2', '5');
+/**
+*   Entidad 1, id_user = 5
+*/
+#INSERT INTO user (`id`, `name`, `email`, `active`, `verified`, `password`, `tmp_pwd`, `points`, `terms`) VALUES ('5', 'Entidad2', 'santiago.alvarez@domoti-sas.com', '1', '1', 'c3951cfdddb5b507d7e4ddb6071a2999f7048edbf5d7c8eeacbb820fd1960868', '1', '15', '1');
+#INSERT INTO institution (`id`, `name`, `nit`, `email`, `phone`, `extension_phone`, `legalrep_name`, `legalrep_secondname`, `legalrep_lastname`, `legalrep_secondlastname`, `legalrep_typedoc`, `legalrep_document`, `legalrep_email`, `id_region`) VALUES ('2', 'Entidad2', '22334455', '12332', 'santiago.alvarez@domoti-sas.com', '3101111111', 'Pepito', 'Ramón', 'Perez', 'Espitia', '1', '1010000000', 'santiago@gmail.com', '1');
+#INSERT INTO institution_user (`id_institution`,`id_user`) VALUES ('2', '5');
+#INSERT INTO user_role (`id_user`,`id_role`) VALUES ('5','4');
 
 /**
 *	Servicios
