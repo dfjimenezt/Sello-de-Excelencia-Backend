30e71d90bed4e5c5ef24117291cac07d5be23d09
Daniel Jiménez <daniel@domoti.rocks>
2017-09-22 09:41:29 -0500

    hall of fame

--- app/models/hall_of_fame.js
+++ app/models/hall_of_fame.js
@@ -20,12 +20,12 @@ var Hall_of_fame = function () {
 		'LEFT JOIN `stamp`.`user` `u` on `u`.`id` = `p`.`id_user` '+
 		'JOIN `stamp`.`user_role` `u_r` on `u_r`.`id_user` = `u`.`id` '+
 		'WHERE `u_r`.`id_role` = 2 GROUP BY `p`.`id_user` ORDER BY points desc LIMIT 10 );'+
-		'INSERT INTO `stamp`.`hall_of_fame` (`points`,`date`,`name`,`id_user`,`id_role`)  ( '+
-		'SELECT SUM(`p`.`value`) points, CURRENT_DATE() date, `u`.`name`,`u`.`id`,`u_r`.`id_role` '+
+
+		'INSERT INTO `stamp`.`hall_of_fame` (`points`,`date`,`name`,`id_institution`,`id_role`)  ( '+
+		'SELECT SUM(`p`.`value`) points, CURRENT_DATE() date, `i`.`name`,`i`.`id`,4 '+
 		'FROM `stamp`.`points` `p` '+
-		'LEFT JOIN `stamp`.`user` `u` on `u`.`id` = `p`.`id_user` '+
-		'JOIN `stamp`.`user_role` `u_r` on `u_r`.`id_user` = `u`.`id` '+
-		'WHERE `u_r`.`id_role` = 4 GROUP BY `p`.`id_user` ORDER BY points desc LIMIT 10 );'
+		'LEFT JOIN `stamp`.`institution` `i` on `i`.`id` = `p`.`id_institution` '+
+		'GROUP BY `p`.`id_institution` ORDER BY points desc LIMIT 10 );'
 		return this.customQuery(q)
 	}
 	return this
